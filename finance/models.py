79fb0b9098ea94c0b010cff8163bf152b9e165ed
Anton1912 <g.goreslavskyi19@gmail.com>
2020-10-29 01:43:53 +0200

    create currency catalogue

---
+++ finance/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=100)
+    abbreviation = models.CharField(max_length=50)
+    symbol = models.CharField(max_length=100, null=True)
+    index = models.PositiveIntegerField(null=True)
+
+
+class Countries(models.Model):
+    country = models.CharField(max_length=100)
+    index = models.PositiveIntegerField(null=True)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+
+
+class Places(models.Model):
+    type_place = models.CharField(max_length=100)
+
+
+class MoneyTransaction(models.Model):
+    type_transaction = models.CharField(max_length=50)
+
+
+class BankStatements(models.Model):
+    transaction_place = models.CharField(max_length=200)
+    type_place = models.ForeignKey(Places, on_delete=models.CASCADE, null=True)
+    type_transaction = models.ForeignKey(MoneyTransaction, on_delete=models.CASCADE)
+    sum_transaction = models.DecimalField(max_digits=20, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    country = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    date_of_trans = models.DateTimeField(null=True)
+
+
--- gsite/settings/settings.py
+++ gsite/settings/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'finance.apps.FinanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
