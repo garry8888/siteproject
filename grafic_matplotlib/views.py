22234e4dac237a663bd9459efdc127e1375b5d7d
Anton1912 <g.goreslavskyi19@gmail.com>
2021-01-23 13:45:22 +0200

    .

--- grafic_matplotlib/views.py
+++ grafic_matplotlib/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 from grafic_matplotlib.figures import pie
-from grafic_matplotlib.models import Pie
 
 
 def index(request):
