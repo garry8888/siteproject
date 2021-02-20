d27f16fe2eb0f1e7a1ca50899a7cf8a965d85d80
Anton1912 <g.goreslavskyi19@gmail.com>
2021-02-20 17:39:23 +0200

    fix styles

--- factory/forms.py
+++ factory/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 
+# загрузка данных с гуглдок
 class UrlForm(forms.URLInput):
     input_type = 'url'
 
@@ -10,5 +11,5 @@ class SheetNumber(forms.NumberInput):
 
 
 class UrlInput(forms.Form):
-    url_field = forms.URLField(widget=UrlForm)
-    sheet_number = forms.IntegerField(widget=SheetNumber)
+    url_field = forms.URLField(label='Введите URL', widget=UrlForm)
+    sheet_number = forms.IntegerField(label='Введите номер страницы', widget=SheetNumber)
